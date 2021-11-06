0709066c1123a289a3b324bdf994fd6853ff1099
Sonmusic <64028850+Sonmusic@users.noreply.github.com>
2021-11-05 23:17:32 -0400

    Add project files.

---
+++ FunctionAndRelation/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionAndRelation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A
+            string answ1 = "s";
+            List<int> A = new List<int>();
+            while (answ1 == "s")
+            {
+                Console.WriteLine("Introduzca el número del conjunto A");
+                A.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Desea continuar? {s/n}");
+                answ1 = Console.ReadLine();
+            }
+            Console.Clear();
+
+            //B
+            string answ3 = "s";
+            List<int> B = new List<int>();
+            while (answ3 == "s")
+            {
+                Console.WriteLine("Introduzca el número del conjunto B");
+                B.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Desea continuar? {s/n}");
+                answ3 = Console.ReadLine();
+            }
+            Console.Clear();
+
+            //printear
+            Console.WriteLine("A:");
+            foreach (var x in A)
+            {
+                Console.Write(x + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("B:");
+            foreach (var x in B)
+            {
+                Console.Write(x + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+            //dominio
+            string answ = "s";
+            List<int> dominio = new List<int>();
+            while (answ == "s")
+            {
+                Console.WriteLine("Introduzca el número del dominio");
+                dominio.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Desea continuar? {s/n}");
+                answ = Console.ReadLine();
+            }
+            Console.Clear();
+
+            //rango
+            string answ2 = "s";
+            List<int> rango = new List<int>();
+            while (answ2 == "s")
+            {
+                Console.WriteLine("Introduzca el número del rango");
+                rango.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Desea continuar? {s/n}");
+                answ2 = Console.ReadLine();
+            }
+            Console.Clear();
+
+            //printear
+            Console.WriteLine();
+            Console.WriteLine("dominio:");
+            foreach (var x in dominio)
+            {
+                Console.Write(x + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("rango:");
+            foreach (var x in rango)
+            {
+                Console.Write(x + ", ");
+            }
+            Console.WriteLine();
+
+            //Algoritmo para determinar si es una relación o una función
+            var a = A.All(dominio.Contains) && A.Count == dominio.Count;
+            if (a)
+            {
+                Console.WriteLine("Es una relación");
+                if(dominio.Count >= rango.Count)
+                {
+                    Console.WriteLine("Es una función");
+                }
+                else
+                {
+                    Console.WriteLine("No es una función");
+                }
+
+            }
+            else { Console.WriteLine("No es una relación"); }
+        }
+    }
+}
---
+++ Semana1Tendencias/Program.cs
@@ -0,0 +1,35 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 2 SnakeCase
+            List<string> stringArray1 = new List<string>();
+            Console.WriteLine("Introduce una palabra:");
+            stringArray1.Add(Console.ReadLine());
+            Console.WriteLine("Introduce otra palabra:");
+            stringArray1.Add(Console.ReadLine());
+            string concatenated ="";
+            string converted;
+            string separator = "";
+            foreach (string x in stringArray1)
+            {
+                converted = string.Empty;
+                foreach (char c in x)
+                {
+                    converted += (Char.ToLower(c));
+                }
+                concatenated += separator + (converted);
+                separator = "_";
+            }
+            Console.WriteLine("SnakeCase: " + concatenated);
+        }
+    }
+}
